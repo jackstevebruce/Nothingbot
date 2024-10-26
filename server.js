5d846870c847d6faabc4188068339e1b2bd63f9c
Jackstevebruce <jackstevebruce@gmail.com>
2024-10-26 08:50:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.post('/api/imitate', async (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://git:${port}`);
 });
